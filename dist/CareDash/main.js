73b9ef0fc656eca4af6f11194b5955232946e631
Lisa Dang <xlisatdang@gmail.com>
2018-09-09 16:16:22 -0400

    update distributables

--- dist/CareDash/main.js
+++ dist/CareDash/main.js
@@ -41,7 +41,7 @@ module.exports = ".container {\r\n    width: 75%;\r\n    margin: 20px auto 0 aut
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<cd-navbar></cd-navbar>\r\n\r\n<div class=\"container\">\r\n  <div fxLayout=\"row\" fxLayoutGap=\"15px\">\r\n    <mat-card class=\"content\" fxFlex=\"70\">\r\n      content\r\n    </mat-card>\r\n  \r\n    <cd-sign-up fxFlex></cd-sign-up>\r\n  </div>\r\n</div>"
+module.exports = "<cd-navbar></cd-navbar>\r\n\r\n<div class=\"container\">\r\n  <div fxLayout=\"row\" fxLayoutGap=\"15px\">\r\n    <mat-card class=\"content\" fxFlex=\"70\">\r\n      content\r\n    </mat-card>\r\n  \r\n    <cd-sign-up fxFlex></cd-sign-up>\r\n  </div>\r\n\r\n  <h2 class=\"center-text\">Reasons to Sign Up as a Provider on CareDash</h2>\r\n\r\n  <div fxLayout=\"row\" fxLayoutGap=\"50px\">\r\n    <cd-headline fxFlex></cd-headline>\r\n    <cd-headline fxFlex></cd-headline>\r\n    <cd-headline fxFlex></cd-headline>\r\n  </div>\r\n</div>\r\n\r\n<cd-footer></cd-footer>"
 
 /***/ }),
 
@@ -100,12 +100,16 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
 /* harmony import */ var _components_navbar_navbar_component__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./components/navbar/navbar.component */ "./src/app/components/navbar/navbar.component.ts");
 /* harmony import */ var _components_sign_up_sign_up_component__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./components/sign-up/sign-up.component */ "./src/app/components/sign-up/sign-up.component.ts");
+/* harmony import */ var _components_headline_headline_component__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./components/headline/headline.component */ "./src/app/components/headline/headline.component.ts");
+/* harmony import */ var _components_footer_footer_component__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./components/footer/footer.component */ "./src/app/components/footer/footer.component.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+
+
 
 
 
@@ -123,7 +127,9 @@ var AppModule = /** @class */ (function () {
             declarations: [
                 _app_component__WEBPACK_IMPORTED_MODULE_6__["AppComponent"],
                 _components_navbar_navbar_component__WEBPACK_IMPORTED_MODULE_7__["NavbarComponent"],
-                _components_sign_up_sign_up_component__WEBPACK_IMPORTED_MODULE_8__["SignUpComponent"]
+                _components_sign_up_sign_up_component__WEBPACK_IMPORTED_MODULE_8__["SignUpComponent"],
+                _components_headline_headline_component__WEBPACK_IMPORTED_MODULE_9__["HeadlineComponent"],
+                _components_footer_footer_component__WEBPACK_IMPORTED_MODULE_10__["FooterComponent"]
             ],
             imports: [
                 _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
@@ -137,6 +143,132 @@ var AppModule = /** @class */ (function () {
         })
     ], AppModule);
     return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/components/footer/footer.component.css":
+/*!********************************************************!*\
+  !*** ./src/app/components/footer/footer.component.css ***!
+  \********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ "./src/app/components/footer/footer.component.html":
+/*!*********************************************************!*\
+  !*** ./src/app/components/footer/footer.component.html ***!
+  \*********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<p>\r\n  footer works!\r\n</p>\r\n"
+
+/***/ }),
+
+/***/ "./src/app/components/footer/footer.component.ts":
+/*!*******************************************************!*\
+  !*** ./src/app/components/footer/footer.component.ts ***!
+  \*******************************************************/
+/*! exports provided: FooterComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "FooterComponent", function() { return FooterComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var FooterComponent = /** @class */ (function () {
+    function FooterComponent() {
+    }
+    FooterComponent.prototype.ngOnInit = function () {
+    };
+    FooterComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'cd-footer',
+            template: __webpack_require__(/*! ./footer.component.html */ "./src/app/components/footer/footer.component.html"),
+            styles: [__webpack_require__(/*! ./footer.component.css */ "./src/app/components/footer/footer.component.css")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], FooterComponent);
+    return FooterComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/components/headline/headline.component.css":
+/*!************************************************************!*\
+  !*** ./src/app/components/headline/headline.component.css ***!
+  \************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "div.headline-img {\r\n    width: 200px;\r\n    height: 120px;\r\n    border: 1px solid #000;\r\n    margin: auto;\r\n    background-color: #fff;\r\n}\r\n\r\n.headline {\r\n    text-align: center;\r\n}"
+
+/***/ }),
+
+/***/ "./src/app/components/headline/headline.component.html":
+/*!*************************************************************!*\
+  !*** ./src/app/components/headline/headline.component.html ***!
+  \*************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"headline\">\r\n  <div class=\"headline-img\"></div>\r\n  \r\n  <h3>Headline</h3>\r\n</div>\r\n\r\n<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean congue elementum dui id euismod. Duis tortor lorem,\r\neuismod sed dapibus sed, finibus at lacus. Curabitur venenatis consequat interdum. Etiam neque lacus, rutrum non\r\nlaoreet a, gravida vitae justo. Vivamus ultricies felis at sem laoreet, condimentum fermentum augue ornare.</div>"
+
+/***/ }),
+
+/***/ "./src/app/components/headline/headline.component.ts":
+/*!***********************************************************!*\
+  !*** ./src/app/components/headline/headline.component.ts ***!
+  \***********************************************************/
+/*! exports provided: HeadlineComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "HeadlineComponent", function() { return HeadlineComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var HeadlineComponent = /** @class */ (function () {
+    function HeadlineComponent() {
+    }
+    HeadlineComponent.prototype.ngOnInit = function () {
+    };
+    HeadlineComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'cd-headline',
+            template: __webpack_require__(/*! ./headline.component.html */ "./src/app/components/headline/headline.component.html"),
+            styles: [__webpack_require__(/*! ./headline.component.css */ "./src/app/components/headline/headline.component.css")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], HeadlineComponent);
+    return HeadlineComponent;
 }());
 
 
@@ -213,7 +345,7 @@ var NavbarComponent = /** @class */ (function () {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = ".sign-up-header {\r\n    text-align: center;\r\n}"
+module.exports = ""
 
 /***/ }),
 
@@ -224,7 +356,7 @@ module.exports = ".sign-up-header {\r\n    text-align: center;\r\n}"
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<mat-card>\r\n  <h2 class=\"sign-up-header\">Sign Up with CareDash to Edit your Provider Profile</h2>\r\n\r\n  <form fxLayout=\"column\">\r\n    <mat-form-field>\r\n      <input type=\"text\" matInput placeholder=\"First Name\" required>\r\n    </mat-form-field>\r\n  \r\n    <mat-form-field>\r\n      <input type=\"text\" matInput placeholder=\"Last Name\" required>\r\n    </mat-form-field>\r\n  \r\n    <mat-form-field>\r\n      <input type=\"text\" matInput placeholder=\"Email Address\" required>\r\n    </mat-form-field>\r\n  \r\n    <mat-form-field>\r\n      <input type=\"password\" matInput placeholder=\"Password\" required>\r\n    </mat-form-field>\r\n  \r\n    <mat-form-field>\r\n      <input type=\"password\" matInput placeholder=\"Confirm Password\" required>\r\n    </mat-form-field>\r\n\r\n    <div fxLayout=\"row\" fxLayoutAlign=\"end center\" fxLayoutGap=\"10px\">\r\n      <button mat-raised-button color=\"primary\">Sign Up</button>\r\n      <button mat-raised-button>Login</button>\r\n    </div>\r\n  </form>\r\n</mat-card>"
+module.exports = "<mat-card>\r\n  <h2 class=\"center-text\">Sign Up with CareDash to Edit your Provider Profile</h2>\r\n\r\n  <form fxLayout=\"column\">\r\n    <mat-form-field>\r\n      <input type=\"text\" matInput placeholder=\"First Name\" required>\r\n    </mat-form-field>\r\n  \r\n    <mat-form-field>\r\n      <input type=\"text\" matInput placeholder=\"Last Name\" required>\r\n    </mat-form-field>\r\n  \r\n    <mat-form-field>\r\n      <input type=\"text\" matInput placeholder=\"Email Address\" required>\r\n    </mat-form-field>\r\n  \r\n    <mat-form-field>\r\n      <input type=\"password\" matInput placeholder=\"Password\" required>\r\n    </mat-form-field>\r\n  \r\n    <mat-form-field>\r\n      <input type=\"password\" matInput placeholder=\"Confirm Password\" required>\r\n    </mat-form-field>\r\n\r\n    <div fxLayout=\"row\" fxLayoutAlign=\"end center\" fxLayoutGap=\"10px\">\r\n      <button mat-raised-button color=\"primary\">Sign Up</button>\r\n      <button mat-raised-button>Login</button>\r\n    </div>\r\n  </form>\r\n</mat-card>"
 
 /***/ }),
 
